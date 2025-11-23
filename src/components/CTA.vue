83985222814366c36192fa14e92c58a24a9a652c
Andi Naufal Nurfadhil <111062395+andinaufal120@users.noreply.github.com>
2025-11-23 22:31:18 +0800

    feat: add CTA component with join button

---
+++ src/components/CTA.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <div
+    class="grid grid-cols-1 gap-12 bg-linear-to-t from-[#60935D] to-[#111D4A] px-5 py-20 text-center text-white md:px-36 lg:px-72"
+  >
+    <h2 class="text-3xl font-bold">
+      Start your journey to unlock your potential
+    </h2>
+    <p>
+      Discover your strengths and grow with peers who share the same passion for
+      technology.
+    </p>
+    <a href="/join">
+      <button
+        class="transform rounded-lg bg-black px-4 py-2 font-bold text-white transition hover:cursor-pointer hover:bg-white hover:text-black hover:shadow-lg hover:shadow-black/5"
+      >
+        Join now
+      </button>
+    </a>
+  </div>
+</template>
