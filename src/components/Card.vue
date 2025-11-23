e160ff3c8400c27b031a49a21504ef82ac4d2580
Andi Naufal Nurfadhil <111062395+andinaufal120@users.noreply.github.com>
2025-11-23 14:56:47 +0800

    feat: add Card component with image and content display

---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+    content: Object,
+});
+</script>
+
+<template>
+  <div class="overflow-hidden rounded-xl bg-white shadow-md">
+    <div class="md:flex">
+      <div class="md:shrink-0">
+        <img
+          class="h-48 w-full object-cover md:h-full md:w-48"
+          :src="`/src/assets/${content.thumbnail}`"
+          :alt="content.thumbnailAlt" />
+      </div>
+      <div class="p-8">
+        <a href="#" class="mt-1 block font-serif text-lg leading-tight font-medium text-black hover:underline"><h3>{{ content.title }}</h3></a>
+        <p class="mt-2 font-serif text-gray-500">{{ content.description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
